5f21d009ec3ecfacbb119a30299cfb21271b1f28
yeye <purielgonzaga@gmail.com>
2025-04-15 21:06:52 +0800

    Update GamePanel.java

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
-import java.io.File;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
@@ -37,18 +35,17 @@ public class GamePanel extends JPanel {
     // drawn in the panel
     public void paintComponent(Graphics g) {
         try {
-            Image img = ImageIO.read(new File("res\\sprites\\pasta.png"));
-            g.drawImage(img, 0, 0, this);
+
         } catch (Exception e) {
 
         }
-        // super.paintComponent(g);
+        super.paintComponent(g);
 
-        // Graphics2D g2 = (Graphics2D) g;
+        Graphics2D g2 = (Graphics2D) g;
 
-        // g2.setColor(Color.white);
+        g2.setColor(Color.white);
 
-        // g2.fillRect(0, 100, TILESIZE, TILESIZE);
-        // g2.dispose();
+        g2.fillRect(0, 100, TILESIZE, TILESIZE);
+        g2.dispose();
     }
 }
