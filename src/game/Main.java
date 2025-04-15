f653e539704d2b52c1e4e1f9662821087a0680ae
yeye <purielgonzaga@gmail.com>
2025-04-15 17:55:25 +0800

    nag try simula

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,7 +1,15 @@
 package game;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Game");
 
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
