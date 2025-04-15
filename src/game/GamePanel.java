0cfeff3f77e2ecd64a9f766a36e08ceddf2ee9a1
yeye <purielgonzaga@gmail.com>
2025-04-15 20:37:21 +0800

    setup the gamePanel
    just the basic stuff/ initial stuff

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -1,7 +1,22 @@
 package game;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
+    // SCREEN SETTINGS
+    final int ORIGINALTILESIZE = 16; // 16x16 tile
+    final int SCALE = 3;
+    final int TILESIZE = ORIGINALTILESIZE * SCALE; // 48x48 tile
+    final int MAXSCREENCOL = 16;
+    final int MAXSCREENROW = 12;
+    final int SCREENWIDTH = TILESIZE * MAXSCREENCOL; // 768 pixel
+    final int SCREENHEIGHT = TILESIZE * MAXSCREENROW; // 576 pixel
 
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(SCREENWIDTH, SCREENHEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
 }
--- src/game/Main.java
+++ src/game/Main.java
@@ -5,9 +5,14 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
+        GamePanel gamePanel = new GamePanel();
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Game");
+
+        window.add(gamePanel);
+        window.pack();
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
